97dfb9c292b163eadf7be5698cf6500c43d2d62b
Garrick Suemith <garrick.suemith@gmail.com>
2021-02-28 23:04:17 -0500

    added routing and navlinks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-
+import { Route, Switch } from 'react-router-dom'
 import './App.css';
 
 import Header from './components/Header'
+
 import Home from './routes/Home'
-import SkillSet from './components/SkillSet'
+import About from './routes/About'
+import Portfolio from './routes/Portfolio'
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Home />
+      <Switch>
+        <Route path="/about" component={About}/>
+        <Route path="/portfolio" component={Portfolio}/>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
       
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,17 @@
 import logo from '../static/images/logo.svg';
 
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
   return (
     <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <nav>
-          <a>Home</a>
-          <a>About</a>
-          <a>Portfolio</a>
-          <a>Contact</a>
+          <NavLink exact to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/portfolio">Portfolio</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
         </nav>
        
       </header>
