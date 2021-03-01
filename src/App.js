57cddd58ec1673956762b2bd492aaa4cee623762
Garrick Suemith <garrick.suemith@gmail.com>
2021-02-28 21:11:06 -0500

    add skillset components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import Header from './components/Header'
 import Home from './routes/Home'
+import SkillSet from './components/SkillSet'
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Home />
+      <SkillSet />
     </div>
   );
 }
---
+++ src/components/SkillSet.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import SkillCard from './SkillCard'
+
+const SkillSet = () => {
+  return (
+    <div className="skillset">
+      
+    </div>
+  )
+}
+
+export default SkillSet
